8087ef3513ef0f18741b01ace2df1dea77298397
Sachin Ayon Fernando <sachinayon@gmail.com>
2024-07-14 21:54:55 +0530

    Added soft delete option

--- database/migrations/2024_07_14_155911_create_books_table.php
+++ database/migrations/2024_07_14_155911_create_books_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 return new class extends Migration
 {
@@ -16,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('author');
             $table->text('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
